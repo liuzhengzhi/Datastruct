331bd2c6e9fc764a1914e61f437414fafa339be6
ice_city <liuhiter@gmail.com>
2015-12-25 13:04:46 +0800

    modified:   eg_1_1_1.cpp

--- eg_1_1_1.cpp
+++ eg_1_1_1.cpp
@@ -1,5 +1,6 @@
 #include "eg_1_1_1.h"
 
+//add student
 void add_student(Student& rhs)
 {
 	for(auto i = &root; ; i=i->next)
@@ -15,6 +16,7 @@ void add_student(Student& rhs)
 	}	
 }
 
+//search student by their name or id number
 void search_student(string& rhs)
 {
 	for(auto i = (&root)->next; i != NULL; i = i->next)
@@ -27,3 +29,21 @@ void search_student(string& rhs)
 	}
 	cout << "No such student" <<endl;
 }
+
+//delete student infomation by name or id number
+void delete_student(string& rhs)
+{
+	for(auto i = (&root)->next , j = &root; i != NULL; i=i->next,j=j->next)
+	{
+		
+		if(i->name == rhs || i->IDnumber == rhs)
+		{
+			cout << "Delete" << i->name <<" " << i->IDnumber << " "<< i->math << i->english << " " << i->chinese << endl;
+			j->next = i->next;
+			delete i;
+			return;
+		}
+	}
+}
+
+//How to sort will be shown in the last homework.
